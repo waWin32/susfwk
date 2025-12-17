dda4f7b9c293c92674a7ced73434ec57281a7944
Kazakoff <simd256bit@gmail.com>
2025-12-18 01:27:14 +0300

    refactor(math): clean up and standardize type definitions

--- include/susfwk/math.h
+++ include/susfwk/math.h
@@ -38,36 +38,36 @@ SUS_INLINE VOID SUSAPI sus_srand(sus_u32 seed) {
 #define sus_randint(a, b) ((sus_rand() % ((b) - (a) + 1)) + (a))
 
 // Rounding down
-SUS_INLINE sus_i32 SUSAPI sus_floor(sus_f32 x) {
-    return (x >= 0.0f) ? (sus_i32)x : (sus_i32)(x - 1);
+SUS_INLINE sus_int SUSAPI sus_floor(sus_float x) {
+    return (x >= 0.0f) ? (sus_int)x : (sus_int)(x - 1);
 }
 // Rounding up
-SUS_INLINE sus_i32 SUSAPI sus_ceil(sus_f64 x) {
-    return (x > 0.0f) ? (sus_i32)(x + 1) : (sus_i32)x;
+SUS_INLINE sus_int SUSAPI sus_ceil(sus_float x) {
+    return (x > 0.0f) ? (sus_int)(x + 1) : (sus_int)x;
 }
 // The number module
-SUS_INLINE sus_i32 SUSAPI sus_abs(sus_i32 x) {
+SUS_INLINE sus_int SUSAPI sus_abs(sus_int x) {
     return (x < 0) ? -x : x;
 }
 // The module of a floating-point number
-SUS_INLINE sus_f32 SUSAPI sus_fabs(sus_f32 x) {
+SUS_INLINE sus_double SUSAPI sus_fabs(sus_double x) {
     return (x < 0.0f) ? -x : x;
 }
 // The module of a floating-point number
-SUS_INLINE sus_f64 SUSAPI sus_dabs(sus_f64 x) {
+SUS_INLINE sus_float SUSAPI sus_fabsf(sus_float x) {
     return (x < 0.0f) ? -x : x;
 }
 // Converting degrees to radians
-SUS_INLINE sus_f32 SUSAPI sus_radians(sus_f32 angle) {
+SUS_INLINE sus_float SUSAPI sus_radians(sus_float angle) {
     return angle * SUS_PI / 180.0f;
 }
 // The remainder of the division
-SUS_INLINE sus_f32 SUSAPI sus_fmod(sus_f32 a, sus_f32 b) {
-    sus_f32 quotient = a / b;
+SUS_INLINE sus_float SUSAPI sus_fmodf(sus_float a, sus_float b) {
+    sus_float quotient = a / b;
     return (quotient >= 0) ? (a - sus_floor(quotient) * b) : (a - sus_ceil(quotient) * b);
 }
 // Tabular sine
-SUS_INLINE sus_f32 SUSAPI sus_sin(sus_i32 degrees) {
+SUS_INLINE sus_float SUSAPI sus_sin(sus_int degrees) {
     degrees %= 360;
     if (degrees < 0) degrees += 360;
     if (degrees <= 90) return SUS_COS_TABLE[90 - degrees];
@@ -76,7 +76,7 @@ SUS_INLINE sus_f32 SUSAPI sus_sin(sus_i32 degrees) {
     return -SUS_COS_TABLE[degrees - 270];
 }
 // Tabular cosine
-SUS_INLINE sus_f32 SUSAPI sus_cos(sus_i32 degrees) {
+SUS_INLINE sus_float SUSAPI sus_cos(sus_int degrees) {
     degrees %= 360;
     if (degrees < 0) degrees += 360;
     if (degrees <= 90) return SUS_COS_TABLE[degrees];
@@ -85,10 +85,10 @@ SUS_INLINE sus_f32 SUSAPI sus_cos(sus_i32 degrees) {
     return SUS_COS_TABLE[360 - degrees];
 }
 // Tabular acosine
-SUS_INLINE sus_f32 SUSAPI sus_acos(sus_f32 x) {
+SUS_INLINE sus_float SUSAPI sus_acos(sus_int x) {
     if (x >= 1.0f) return 0.0f;
     if (x <= -1.0f) return 180.0f;
-    sus_f32 sign = 1.0f;
+    sus_float sign = 1.0f;
     if (x < 0.0f) { x = -x; sign = -1.0f; };
     sus_int i = 0;
     while (i < 90 && SUS_COS_TABLE[i] > x) i++;
@@ -96,18 +96,18 @@ SUS_INLINE sus_f32 SUSAPI sus_acos(sus_f32 x) {
     return sign * ((i - 1) + (x - SUS_COS_TABLE[i]) / (SUS_COS_TABLE[i - 1] - SUS_COS_TABLE[i]));
 }
 // Tabular asine
-SUS_INLINE sus_f32 SUSAPI sus_asin(sus_i32 x) {
-    return 90.0f - sus_acos(sus_clamp(x, -1.0f, 1.0f));
+SUS_INLINE sus_float SUSAPI sus_asin(sus_int x) {
+    return 90.0f - sus_acos(sus_clamp(x, -1, 1));
 }
 // Get the tangent of the angle
-SUS_INLINE sus_float SUSAPI sus_tan(sus_i32 x) {
+SUS_INLINE sus_float SUSAPI sus_tan(sus_int x) {
     return sus_sin(x) / sus_cos(x);
 }
 // Get the square root of a number
-SUS_INLINE sus_f32 SUSAPI sus_sqrt(sus_f32 x) {
+SUS_INLINE sus_float SUSAPI sus_sqrt(sus_float x) {
     if (x <= 0.0f) return 0.0f;
-    sus_f32 guess = x;
-    sus_f32 next_guess;
+    sus_float guess = x;
+    sus_float next_guess;
     for (int i = 0; i < SUS_MAX_ITER_SQRT; i++) {
         next_guess = (guess + x / guess) / 2.0f;
         if (sus_fabs(next_guess - guess) < SUS_EPSILON) {
@@ -118,9 +118,9 @@ SUS_INLINE sus_f32 SUSAPI sus_sqrt(sus_f32 x) {
     return guess;
 }
 // Raise a number to a power
-SUS_INLINE sus_f32 SUSAPI sus_pow(sus_f32 x, sus_i32 exp) {
+SUS_INLINE sus_float SUSAPI sus_pow(sus_float x, sus_int exp) {
     if (exp < 0) return 1.0f / sus_pow(x, -exp);
-    sus_f32 rez = 1.0f;
+    sus_float rez = 1.0f;
     while (exp > 0) {
         if (exp % 2 == 1) {
             rez *= x;
@@ -131,12 +131,12 @@ SUS_INLINE sus_f32 SUSAPI sus_pow(sus_f32 x, sus_i32 exp) {
     return rez;
 }
 // Arctangent using the approximation
-SUS_INLINE sus_f32 SUSAPI sus_atan(sus_f32 x) {
-    sus_f32 x2 = x * x;
+SUS_INLINE sus_float SUSAPI sus_atan(sus_float x) {
+    sus_float x2 = x * x;
     return x * (0.7854f - 0.0804f * x2) / (1.0f - 0.825f * x2);
 }
 // Arctangent2 using for the full range of angles
-SUS_INLINE sus_f32 SUSAPI sus_atan2(sus_f32 y, sus_f32 x) {
+SUS_INLINE sus_float SUSAPI sus_atan2(sus_float y, sus_float x) {
     if (x == 0.0f) {
         if (y > 0.0f) return 90.0f;
         if (y < 0.0f) return -90.0f;
